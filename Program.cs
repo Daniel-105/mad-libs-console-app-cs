e1f204dc0334aea79998afcde3c9adae215a4dd8
Daniel-105 <danielpinto105@gmail.com>
2023-06-07 13:13:38 +0100

    Mad libs console application uploaded

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// When?
+// who? 
+// action?
+// What?
+// with who/what?
+// Where?
+// Why?
+Console.Write("When?");
+string when = Console.ReadLine();
+
+Console.Write("Who?");
+string who = Console.ReadLine();
+
+Console.Write("Specify the action");
+string action = Console.ReadLine();
+
+Console.Write("What?");
+string what = Console.ReadLine();
+
+Console.Write("With whom/what?");
+string withWhomOrWhat = Console.ReadLine();
+
+Console.Write("Where?");
+string where = Console.ReadLine();
+
+Console.Write("Why?");
+string why = Console.ReadLine();
+
+
+Console.WriteLine(when);
+Console.WriteLine(who);
+Console.WriteLine(action);
+Console.WriteLine(what);
+Console.WriteLine(withWhomOrWhat);
+Console.WriteLine(where);
+Console.WriteLine(why);
+
+
+
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# Mad Libs console application in C#
+
+This is a fun console application for you to have fun.
+
+You will be asked some questions that will help you write a story. Answer the question in the more creative and imaginative way you can.
+
+Have fun :D
